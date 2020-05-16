439f66853607f991838103d930aaf4186825be77
David Ahmed <david@Davids-MacBook-Pro.local>
2020-05-16 15:09:48 -0400

    Updates

---
+++ precache-manifest.e0e8aa210916aaaced2c09c96d9d31f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ee2264bec858e587fd4d0304527155",
+    "url": "/vision-2020-web/index.html"
+  },
+  {
+    "revision": "e4eb1826ce6707d92000",
+    "url": "/vision-2020-web/static/css/main.be7f58ec.chunk.css"
+  },
+  {
+    "revision": "806b143a75a4e5c6c573",
+    "url": "/vision-2020-web/static/js/2.a92a7c99.chunk.js"
+  },
+  {
+    "revision": "7f3d623d03408f9350c3b5e194511d01",
+    "url": "/vision-2020-web/static/js/2.a92a7c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4eb1826ce6707d92000",
+    "url": "/vision-2020-web/static/js/main.3afbcf1b.chunk.js"
+  },
+  {
+    "revision": "c5c3dcf21a35b8ff1f63",
+    "url": "/vision-2020-web/static/js/runtime-main.a85d3028.js"
+  }
+]);
