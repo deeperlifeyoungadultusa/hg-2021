bfc8c65988c2e4533571bc07428d85f2b2d660d0
David Ahmed <david@Davids-MacBook-Pro.local>
2020-05-16 15:07:55 -0400

    Updates

---
+++ precache-manifest.b5109715a1a8e180c1f01f95d4f3b238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125a8db9f0af720f109ce3c7ce0d24ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d660d6905af970f5f5f7",
+    "url": "/static/css/main.be7f58ec.chunk.css"
+  },
+  {
+    "revision": "d117cdbd9c8ec681273e",
+    "url": "/static/js/2.f09d4608.chunk.js"
+  },
+  {
+    "revision": "7f3d623d03408f9350c3b5e194511d01",
+    "url": "/static/js/2.f09d4608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d660d6905af970f5f5f7",
+    "url": "/static/js/main.3c7900fe.chunk.js"
+  },
+  {
+    "revision": "f238882d034d371b887e",
+    "url": "/static/js/runtime-main.86672f24.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vision-2020-web/precache-manifest.e0e8aa210916aaaced2c09c96d9d31f5.js"
+  "/precache-manifest.b5109715a1a8e180c1f01f95d4f3b238.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vision-2020-web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
